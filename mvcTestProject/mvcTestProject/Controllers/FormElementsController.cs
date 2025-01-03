e48f292a6dc6f712ab4ca060996315f365c67897
unknown <Administrator@test.okyanus.local>
2025-01-03 16:46:13 +0300

    first

---
+++ mvcTestProject/mvcTestProject/Controllers/FormElementsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvcTestProject.Models;
+
+namespace mvcTestProject.Controllers
+{
+    public class FormElementsController : Controller
+    {
+        private readonly ILogger<FormElementsController> _logger;
+
+        public FormElementsController(ILogger<FormElementsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mvcTestProject/mvcTestProject/Controllers/SharedController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class SharedController
+{
+	public SharedController()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
